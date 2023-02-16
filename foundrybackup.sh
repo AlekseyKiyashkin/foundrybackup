b35d1998f5c669e641c12696ba0c0d023e3dab00
Aleksei Kiiashkin <akiyashkin@gmail.com>
2023-02-16 18:23:49 +0300

    edited variables

--- foundrybackup.sh
+++ foundrybackup.sh
@@ -2,14 +2,17 @@
 
 #set variables
 
-FoundryHomeDir="/home/kroot/foundry/"
-FoundryDataDir="/home/kroot/foundryuserdata/"
-BackupsDir="var/backups/foundry"
+FoundryHomeDir=""
+FoundryDataDir=""
+BackupsDir=""
 LogFileName="$BackupsDir/backup_$(date +"%d_%m_%Y").log"
-ExclusionsFileName="home/kroot/exclusions.txt"
+ExclusionsFileName="exclusions.txt"
+
 
 #backup foundry home
-tar --exclude-from="$ExclusionsFileName" -zcvf $BackupsDir/foundryhome$(date +"%d_%m_%Y").tar.gz $FoundryHomeDir >> $LogFileName
+tar --exclude-from="$ExclusionsFileName" -zcvf $BackupsDir/foundryhome$(date +"%d_%m_%Y").tar.gz $FoundryHomeDir > $LogFileName
+
 
 #backup foundry userdata
-tar --exclude-from="$ExclusionsFileName" -zcvf $BackupsDir/foundryuserdata$(date +"%d_%m_%Y").tar.gz $FoundryHomeDir >> $LogFileName
+tar --exclude-from="$ExclusionsFileName" -zcvf $BackupsDir/foundryuserdata$(date +"%d_%m_%Y").tar.gz $FoundryDataDir >> $LogFileName
+
